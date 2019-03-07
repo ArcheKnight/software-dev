ad654907f067f38ef250e29f401d06b4d7b51b57
Devon Troedel <devon.troedel@gmail.com>
2019-03-07 16:31:16 -0600

    auto commit

--- udemy/SG-ReactRedux/seasons/src/index.jsx
+++ udemy/SG-ReactRedux/seasons/src/index.jsx
@@ -1,35 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: '' };
 
-    // The only time that we directly assign something to this.state
-    //this.state = { lat: null };
-    this.state = { lat: null, errorMessage: '' };
-
-        window.navigator.geolocation.getCurrentPosition(
-          (position) => {
-            // Call setState to update state
-            this.setState({ lat: position.coords.latitude });
-          }, // Success callback
-          (err) => {
-            this.setState({ errorMessage: err.message });
-          } //error callback
-        );
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    )
   }
 
-  // React requires render method
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
-    } else if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
-    } else {
-      return <div>Loading!</div>;
     }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    )
   }
 }
 
