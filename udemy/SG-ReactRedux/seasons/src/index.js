b09e9c8bbd238c782ccedf969dc5fd279368b3db
Devon Troedel <devon.troedel@gmail.com>
2019-03-06 23:01:24 -0600

    auto commit

--- udemy/SG-ReactRedux/seasons/src/index.js
+++ udemy/SG-ReactRedux/seasons/src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    // The only time that we directly assign something to this.state
+    this.state = { lat: null };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => { console.log(position) }, // Success callback
+      (position) => {
+        // Call setState to update state
+        this.setState({ lat: position.coords.latitude });
+      }, // Success callback
       (err) => { console.log(err) } //error callback
     );
+  }
 
-    return <h3>Latitude: </h3>;
+  // React requires render method
+  render() {
+    return <h3>Latitude: {this.state.lat}</h3>;
   }
 }
 
