551d8368c54a79e52f2473503d7cf506f5eb8eef
Devon Troedel <devon.troedel@gmail.com>
2019-03-07 19:59:58 -0600

    auto commit

---
+++ udemy/SG-ReactRedux/pics/v1/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
