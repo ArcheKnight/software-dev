75de24eb78ced2e0ffb110213956a92c16de859a
Devon Troedel <devon.troedel@gmail.com>
2019-03-24 21:40:08 -0500

    auto commit

--- udemy/MS-NodeJSComplete/ShopAppNoSQL/app.js
+++ udemy/MS-NodeJSComplete/ShopAppNoSQL/app.js
@@ -4,28 +4,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {});
+app.use((req, res, next) => {
+	User.findById('5c98233c1c9d440000632943')
+		.then(user => {
+			req.user = new User(user.name, user.email, user.cart, user._id);
+			next();
+		})
+		.catch(err => console.log(err));
+});
 
-// app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 const port = process.env.PORT || 3000;
-mongoConnect(client => {
-	//console.log(client);
+mongoConnect(() => {
 	app.listen(port, () => console.log(`Listening on port ${port}`));
 });
--- udemy/MS-NodeJSComplete/ShopAppNoSQL/models/product.js
+++ udemy/MS-NodeJSComplete/ShopAppNoSQL/models/product.js
@@ -1,27 +1,63 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
+class Product {
+	constructor(title, price, description, imageUrl, id, userId) {
+		this.title = title;
+		this.price = price;
+		this.description = description;
+		this.imageUrl = imageUrl;
+		this._id = id ? new mongodb.ObjectId(id) : null;
+		this.userId = userId;
+	}
 
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
-});
+	save() {
+		const db = getDb();
+		let dbOp;
+		if (this._id) {
+			dbOp = db
+				.collection('products')
+				.updateOne({ _id: this._id }, { $set: this })
+				.then(() => console.log(`Updated Product: ${this._id.toHexString()}`));
+		} else {
+			dbOp = db
+				.collection('products')
+				.insertOne(this)
+				.then(() => console.log(`Added Product: ${this._id.toHexString()}`));
+		}
+		return dbOp;
+	}
+
+	static fetchAll() {
+		const db = getDb();
+		return db
+			.collection('products')
+			.find()
+			.toArray()
+			.then(products => {
+				return products;
+			})
+			.catch(err => console.log(err));
+	}
+
+	static findById(prodId) {
+		const db = getDb();
+		return db
+			.collection('products')
+			.find({ _id: new mongodb.ObjectId(prodId) })
+			.next()
+			.then(product => product)
+			.catch(err => console.log(err));
+	}
+
+	static deleteById(prodId) {
+		const db = getDb();
+		return db
+			.collection('products')
+			.deleteOne({ _id: new mongodb.ObjectId(prodId) })
+			.then(() => console.log(`Deleted Product: ${prodId}`))
+			.catch(err => console.log(err));
+	}
+}
 
 module.exports = Product;
