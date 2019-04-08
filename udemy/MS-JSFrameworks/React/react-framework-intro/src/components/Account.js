b4da7650a896420a4edd71a04213f0a2bfaf06d1
Devon Troedel <devon.troedel@gmail.com>
2019-04-07 22:35:32 -0500

    finished react intro

---
+++ udemy/MS-JSFrameworks/React/react-framework-intro/src/components/Account.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default function(props) {
+	return (
+		<div>
+			<h1>Account Page</h1>
+		</div>
+	);
+}
---
+++ udemy/MS-JSFrameworks/React/react-framework-intro/src/components/User.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Info from './Info';
+
+export default function(props) {
+	return (
+		<div>
+			<h1>User Page</h1>
+			<Info />
+		</div>
+	);
+}
