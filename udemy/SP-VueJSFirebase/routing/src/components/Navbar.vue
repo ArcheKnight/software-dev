2d140444fff2e165f5d9af6cfcfc4d0cf6b6394d
Devon Troedel <devon.troedel@gmail.com>
2019-04-08 17:41:48 -0500

    working on vue lessons

--- udemy/SP-VueJSFirebase/routing/src/components/Navbar.vue
+++ udemy/SP-VueJSFirebase/routing/src/components/Navbar.vue
@@ -8,6 +8,20 @@
         <router-link :to="{ name: 'About' }">About</router-link>
       </li>
     </ul>
+    <h2>User Profiles</h2>
+    <ul>
+      <li v-for="(id, index) in userIds" :key="index">
+        <router-link :to="{ name: 'ViewProfile', params: { user_id: id } }">
+          <span> Profile {{ id }} </span>
+        </router-link>
+      </li>
+    </ul>
+    <h2>Navigation Controls</h2>
+    <ul>
+      <li><button @click="goBack">Go Back</button></li>
+      <li><button @click="goHome">Redirect To Home</button></li>
+      <li><button @click="goForward">Go Forward</button></li>
+    </ul>
   </nav>
 </template>
 
@@ -15,7 +29,20 @@
 export default {
   name: "Navbar",
   data() {
-    return {};
+    return {
+      userIds: ["1", "2", "3", "4"]
+    };
+  },
+  methods: {
+    goHome() {
+      this.$router.push({ name: "Home" });
+    },
+    goBack() {
+      this.$router.go(-1);
+    },
+    goForward() {
+      this.$router.go(1);
+    }
   }
 };
 </script>
@@ -28,6 +55,7 @@ ul {
 
 li {
   display: inline-block;
+  margin: 10px;
 }
 
 a {
