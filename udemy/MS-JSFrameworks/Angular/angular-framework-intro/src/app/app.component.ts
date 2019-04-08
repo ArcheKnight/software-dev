5f347e28a9adfcc32f909dbd937466ba954b3d79
Devon Troedel <devon.troedel@gmail.com>
2019-04-07 23:31:53 -0500

    working on angular intro

---
+++ udemy/MS-JSFrameworks/Angular/angular-framework-intro/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Max';
+  elements: number[] = [];
+
+  onChangeName() {
+    this.name = 'Anna';
+  }
+
+  onAddElement() {
+    this.elements.push(this.elements.length + 1);
+  }
+
+  getColor(element) {
+    return element % 2 === 0 ? 'green' : 'red';
+  }
+}
